aad968d3bb3db2b0f4af31d7d14b2fa734717e0d
Levy Lennon <levy.lennon@outlook.com>
2020-07-23 14:42:59 -0300

    Router, Controller and Model | Booking

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const routes = express.Router();
 const multer = require("multer");
 const uploadConfig = require("./config/upload");
+const upload = multer(uploadConfig);
 
+// Controller
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
 const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
 
-const routes = express.Router();
-const upload = multer(uploadConfig);
-
+// Routers
 routes.post("/sessions", SessionController.store);
 routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 routes.get("/dashboard", DashboardController.show);
+routes.post("/booking/:spot_id/bookings", BookingController.store);
 
 module.exports = routes;
