3f00b9b84fadf11b2be1bf90d1038e1f2d0ee110
Levy Lennon <levy.lennon@outlook.com>
2020-07-23 14:16:41 -0300

    first commit

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,17 @@
+// Show = Listar
+// Store = Criar Sessão
+// Update = Alterar Sessão
+// Destroy = Destruir Sessão
+const User = require("../models/User");
+
+module.exports = {
+  async store(req, res) {
+    const { email } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) user = await User.create({ email });
+    
+    return res.json(user);
+  },
+};
